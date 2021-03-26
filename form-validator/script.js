592424e0522d5a8321916f15509354540cc75a9f
jwatts82 <wattscodes@gmail.com>
2021-03-25 20:56:44 -0700

    add error message

--- form-validator/script.js
+++ form-validator/script.js
@@ -4,3 +4,20 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+//Show input error message
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+//Event listeners
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    if(username.value === '') {
+        showError(username, 'Username is requried');
+    } else {
+        showSuccess(username);
+    }
+});
