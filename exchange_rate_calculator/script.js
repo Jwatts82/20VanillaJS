ab46f9c74e1b72a887f204642d694056f719ba84
jwatts82 <wattscodes@gmail.com>
2021-03-30 20:46:07 -0700

    add dom elements

---
+++ exchange_rate_calculator/script.js
@@ -0,0 +1,10 @@
+const currencyElement_one = document.getElementById('currency-one');
+const currencyElement_two = document.getElementById('currency-two');
+const amountElement_one = document.getElementById('amount-one');
+const amountElement_two = document.getElementById('amount-two');
+
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+function calculate() {}
+
+calculate();
