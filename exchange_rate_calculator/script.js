bbe0c5d853132d924f847e98e51efada8985aad1
jwatts82 <wattscodes@gmail.com>
2021-03-30 20:55:59 -0700

    add fetch

--- exchange_rate_calculator/script.js
+++ exchange_rate_calculator/script.js
@@ -5,6 +5,29 @@ const amountElement_two = document.getElementById('amount-two');
 
 const rateEl = document.getElementById('rate');
 const swap = document.getElementById('swap');
-function calculate() {}
+
+//fetch exchange rates and update the DOM
+function calculate() {
+  const currency_one = currencyElement_one.value;
+  const currency_two = currencyElement_two.value;
+
+  //console.log(currency_one, currency_two);
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+    .then(res => res.json())
+    .then(data => {
+      // console.log(data);
+      const rate = data.rates[currency_two];
+
+      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+      amountElement_two.value = (amountElement_one.value * rate).toFixed(2);
+    });
+}
+
+//Event listeners
+currencyElement_one.addEventListener('change', calculate);
+amountElement_one.addEventListener('input', calculate);
+currencyElement_two.addEventListener('change', calculate);
+amountElement_two.addEventListener('input', calculate);
 
 calculate();
